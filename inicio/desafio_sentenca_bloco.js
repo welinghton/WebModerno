b7ecaf3fe05482e2b021cc733caee84666aeea1e
welinghton <wmg.dtna@gmail.com>
2022-08-01 12:39:48 -0300

    TESTE-2

---
+++ inicio/desafio_sentenca_bloco.js
@@ -0,0 +1,20 @@
+/*
+    Imprima os números ímpares de 01 até 10 e depois os pares de 01 até 10, colocando os códigos
+    dentro de blocos de sentenças separados
+*/
+
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+
+{
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
